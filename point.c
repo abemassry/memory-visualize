ad181e4fb79486f27f1cf88d794acf18b719408a
Abe Massry <a@abemassry.com>
2014-01-21 13:16:18 -0500

    adding point.c to visualize arrays

---
+++ point.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int arr[10];
+  arr[1] = 10;
+  arr[2] = 20;
+  arr[5] = 100;
+  int i;
+  for(i = 0; i <= 5; i++) {
+    printf("|----------|----------|----------\n");
+    printf("|          |          |          \n");
+    printf("| arr[%d]   |  Value   |  %d      \n", i, arr[i]);
+    printf("|&arr[%d]   |  Address |  %p      \n", i, &arr[i]);
+    printf("|          |          |\n");
+  }
+
+  return 0;
+}
